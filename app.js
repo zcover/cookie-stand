3e057010ad658e205b4b697f8604c2b77eca3e71
zcover <zerek.cover@gmail.com>
2019-07-15 16:11:05 -0700

    little change, oriented myself by adding empty customer count and cookie sale arrays

--- app.js
+++ app.js
@@ -16,70 +16,78 @@ ServiceHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm'
 function averagecookiehour(a, b){
     //this is going to take the average number of customers per day, and the average cookies sold per day, to identify the number of cookies sold each hour
     var calcAvgCookieHour = a * b;
-    console.log(`${calcAvgCookieHour} cookies per hour`);
+    for(h=0; h<ServiceHours.length; h++) {
+        console.log(ServiceHours[h]);
+    console.log(`${ServiceHours[h]}: ${calcAvgCookieHour} cookies `);
     return calcAvgCookieHour;
 };
+
+
+}
 
 var pike = {
     name: '1st and Pike',
    minCust: 23, //minimum customers per hour
    maxCust: 65, //maximum customers per hour
-   AvgPurchase: 6.3,
+   avgPurchase: 6.3,
+   customersPerHour: [],
+   salesPerHour: [],
    avgCust: function(){//identifies the average number of customers each hour.
-       var randomnumber = Math.floor(Math.random() * (this.maxCust - this.minCust +1) + this.minCust);
-       console.log(`${this.name} Has ${randomnumber} customers per hour`);
-       console.log(randomnumber);
-       averagecookiehour(randomnumber, this.AvgPurchase);
-   },
-    
+        var randomnumber = Math.floor(Math.random() * (this.maxCust - this.minCust +1) + this.minCust);
+        console.log(`${this.name} Has ${randomnumber} customers per hour`);
+        console.log(this.randomnumber);
+    },
+    averagecookiehour(randomnumber, this.AvgPurchase);
+
 };
-var Seatac = {
-    name: 'SeaTac Airport',
-   minCust: 3,
-   maxCust: 24,
-   avgCust: function(){
-       var randomnumber = Math.floor(Math.random() * (this.maxCust - this.minCust +1) + this.minCust);
-       console.log(this.name);
-       console.log(randomnumber);
-   }
-}
+console.log(pike);
+// var Seatac = {
+//     name: 'SeaTac Airport',
+//    minCust: 3,
+//    maxCust: 24,
+//    avgCust: function(){
+//        var randomnumber = Math.floor(Math.random() * (this.maxCust - this.minCust +1) + this.minCust);
+//        console.log(this.name);
+//        console.log(randomnumber);
+//    }
+// }
 
-var SCenter = {
-    name: 'Seattle Center',
-   minCust: 11,
-   maxCust: 38,
-   avgCust: function(){
-       var randomnumber = Math.floor(Math.random() * (this.maxCust - this.minCust +1) + this.minCust);
-       console.log(this.name);
-       console.log(randomnumber);
-   }
-}
+// var SCenter = {
+//     name: 'Seattle Center',
+//    minCust: 11,
+//    maxCust: 38,
+//    avgCust: function(){
+//        var randomnumber = Math.floor(Math.random() * (this.maxCust - this.minCust +1) + this.minCust);
+//        console.log(this.name);
+//        console.log(randomnumber);
+//    }
+// }
 
-var CapHill = {
-    name: 'Capitol Hill',
-   minCust: 20,
-   maxCust: 38,
-   avgCust: function(){
-       var randomnumber = Math.floor(Math.random() * (this.maxCust - this.minCust +1) + this.minCust);
-       console.log(this.name);
-       console.log(randomnumber);
-   }
-}
+// var CapHill = {
+//     name: 'Capitol Hill',
+//    minCust: 20,
+//    maxCust: 38,
+//    avgCust: function(){
+//        var randomnumber = Math.floor(Math.random() * (this.maxCust - this.minCust +1) + this.minCust);
+//        console.log(this.name);
+//        console.log(randomnumber);
+//    }
+// }
 
-var Alki = {
-    name: 'Alki',
-   minCust: 2,
-   maxCust: 16,
-   avgCust: function(){
-       var randomnumber = Math.floor(Math.random() * (this.maxCust - this.minCust +1) + this.minCust);
-       console.log(this.name);
-       console.log(randomnumber);
-   }
-}
+// var Alki = {
+//     name: 'Alki',
+//    minCust: 2,
+//    maxCust: 16,
+//    avgCust: function(){
+//        var randomnumber = Math.floor(Math.random() * (this.maxCust - this.minCust +1) + this.minCust);
+//        console.log(this.name);
+//        console.log(randomnumber);
+//    }
+// }
 
-var shoplist = [pike, Seatac, SCenter, CapHill, Alki];
+// var shoplist = [pike, Seatac, SCenter, CapHill, Alki];
 
-for (var i=0; i<shoplist.length; i++){
-    shoplist[i].avgCust()
+// for (var i=0; i<shoplist.length; i++){
+//     shoplist[i].avgCust()
 
-}
+// }
