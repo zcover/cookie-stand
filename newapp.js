8d6521d7d4c0afb6bd425763bc7f70502626522d
zcover <zerek.cover@gmail.com>
2019-07-17 12:39:21 -0700

    followed along in class to update newapp.js file with assignment details, allowing it to run a command that creates the table

--- newapp.js
+++ newapp.js
@@ -15,19 +15,57 @@ function Location(LocationName, MinimumCustomerCount, MaximumCustomerCount, Aver
     this.min = MinimumCustomerCount,
     this.max = MaximumCustomerCount,
     this.avgPurchse = AverageCustomerPurchase,
-    this.hourlyCookieSales = HelperFunctionCustomerRandomizer()
+    this.hourlyCookieSales = [],//HelperFunctionCustomerRandomizer()
         
 
-    };
     console.log(this.hourlyCookieSales);
-    allLocations.push(this);
+    allLocations.push(this);  
+};
+//RENDER FUNCTION TO MAKE THE TABLE
+
+Location.prototype.render= function(){
+    this.randCustomerCount();
+    this.randCookieCount();
+
+
+    //create element -tr
+    var trEl = document.createElement('tr');
+    
+    //append tr to tablebody
+    tableBody.appendChiled(trEl);
+    
+    //make my location cell
+        //create a td element
+        var tdEl = document.createElement('td');
+        //give it content: this.name
+        tdEl.textContent = this.name;
+        //append it to tr
+        trEl.appendChild(tdEl);
+
+    //make my data -cookies each hour
+    for (var i = 0; i< serviceHours.length; i++){
+        //1. creating an element -td
+        tdEl = document.createElement('td');
+
+        //2. give it acontent - cookies each hour at i
+        tdEl.textContent = this.hourlyCookieSales[i];
+
+        //3 append it to the DOM -tr
+        trEl.appendChild(tdEl);
+
+    }
+
+    //my totals
+        //create a td element
+        tdEl = document.createElement('td');
+        //give content
+        tdEl.textContent = this.cookieTotal;
+        //append it to the DOM -tr
+        trEl.appendChild(tdEl);
+
+};
 
 
 
-
-
-
-
-
-new Location (`1st and Pike`, 23, 65, 6.5, );   
-console.log(allLocations);
+//Instances
+new Location('1st and Pike', 63, 23, 6.3);
