bd5fa6fd4af2b35e3f6dabf20ca347c7dba11cd1
zcover <zerek.cover@gmail.com>
2019-07-17 15:59:07 -0700

    Constructor function created, added random customer generator.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # cookie-stand
+## Purpose:
+### 1. Designing web app that will receive specific data values:
+<br>
+
+ Location name<br>
+ Expected Hourly *maximum* Customer count<br>
+ Expected hourly *minimum* Customer Count<br>
+ _Average_ of purchases from individual customers <br>
+
+### 2. Return a set of values to the user
+>User will be able to visually see an estimated hourly sales, as well as a daily total
+<br>
+
+* The user will be able to enter new locations with a submission box, or update previously estimated values with more up-to-date information.
--- app.js
+++ app.js
@@ -1,37 +1,73 @@
 'use strict';
 
+
+//--------------------------------------------------------------
+//GLOBAL VARIABLES
+
 //fill array with times
 var openHoursArr = ['6am', '7am', '8am', '9am', '10 am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
+//grabbing the myTable id from html
+var tableBody = document.getElementById('myTable');
+
 //allLocationsArr to hold current and future locations
-var allLocations = [];
+var allLocationsArr = [];
 
+//this array holds the names of all the locations, so they can be later referenced as th data
+var tableHeadersArr = []
 
+//----------------------------------------------------------\\
 
+//Helper Functions---------------------------------
+
+//required for submit pages
+function handleClick(event){
+    event.preventDefault();
+};
 //make prototype to autofill (from instance)
-function location.prototype.fill(name, maxCustomers, minCustomers, avgPurchase);
-this.location = name;
-this.maxCustomers = maxCustomers;
-this.minCustomers = minCustomers;
-this.avgPurchase = avgPerchase;
+function custRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-location.push(this)
+//------------------------------------------------/
+//CONSTUCTOR
+function Locationbio(name, maxCustomers, minCustomers, avgPurchase){
+    //name
+    this.name = name;
+    //max
+    this.max = maxCustomers;
+    //min
+    this.min = minCustomers;
+    //avg
+    this.avg = avgPurchase;
+
+    this.customerArray = [];
+
+    this.averageCustomerCount = 
+    //push into the allLocations array
+    allLocationsArr.push(this);
+        //push location names into header array
+    tableHeadersArr.push(this.name);
 
 
-
-
+};
+//--------------------------------------------------
+// END CONSTRUCTOR
+//for loop to have a random number of customers per hour
+    //create number generator for customers/hour
+    //push random number back into the constructor
+Locationbio.prototype.generateCustomerArray= function(){
+    for(var i = 0; i< openHoursArr.length; i++){
+        var randomNumber = custRandom(this.min, this.max);
+        this.customerArray.push(randomNumber)
+    };
+};
 //Need to:
 
-    //name
-    //min
-    //max
-    //avg
 //the information needs to be grabbed from form id=myForm on html
     //able to receive new location information, and loop into the rest of the page
 
-//create number generator for customers/hour
 
-    //for loop to have a random number of customers per hour
 //total hourly sales
     //for loop for hours the store is open
         //multiplies customers *  avg purchse for openHoursArr.length
@@ -44,5 +80,4 @@ location.push(this)
             //td the amount sold each hour, with a different tr
         //append the data to the html
           
-
 //render page
