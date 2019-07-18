e84f382ef90d9ad5e5e104b9fffdcda997a73485
zcover <zerek.cover@gmail.com>
2019-07-17 17:30:52 -0700

    code works, need to get total for day, and then add table

--- app.js
+++ app.js
@@ -23,61 +23,116 @@ var tableHeadersArr = []
 //required for submit pages
 function handleClick(event){
     event.preventDefault();
+
 };
-//make prototype to autofill (from instance)
-function custRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+function custRandom(minCustomers, maxCustomers) {
+    return Math.floor(Math.random() * (maxCustomers - minCustomers + 1)) + minCustomers;
 };
+//multiplies customers *  avgPurchase purchse for openHoursArr.length
+// function salesPerHour(){
+//     daily
+// }
 
 //------------------------------------------------/
 //CONSTUCTOR
 function Locationbio(name, maxCustomers, minCustomers, avgPurchase){
     //name
     this.name = name;
-    //max
-    this.max = maxCustomers;
-    //min
-    this.min = minCustomers;
-    //avg
-    this.avg = avgPurchase;
+    //maxCustomers
+    this.maxCustomers = maxCustomers;
+    //minCustomers
+    this.minCustomers = minCustomers;
+    //avgPurchase
+    this.avgPurchase = avgPurchase;
 
     this.customerArray = [];
 
-    this.averageCustomerCount = 
+    this.hourlySalesArr = [];
+
     //push into the allLocations array
     allLocationsArr.push(this);
         //push location names into header array
     tableHeadersArr.push(this.name);
 
+    this.generateCustomerArray();
+    this.generateSalesArray();
+
 
 };
 //--------------------------------------------------
 // END CONSTRUCTOR
+
+//--------------------------------------------------------------
+//PROTOTYPES
+
 //for loop to have a random number of customers per hour
-    //create number generator for customers/hour
+//create number generator for customers/hour
     //push random number back into the constructor
+
 Locationbio.prototype.generateCustomerArray= function(){
     for(var i = 0; i< openHoursArr.length; i++){
-        var randomNumber = custRandom(this.min, this.max);
+        var randomNumber = custRandom(this.minCustomers, this.maxCustomers);
         this.customerArray.push(randomNumber)
     };
 };
+Locationbio.prototype.generateSalesArray= function(){
+    for(var i=0; i< openHoursArr.length; i++){
+        var randomNumber = (Math.ceil(this.customerArray[i] * this.avgPurchase))
+        this.hourlySalesArr.push(randomNumber);
+    };
+};
+
+//-------------------------------------------------------------------
 //Need to:
 
 //the information needs to be grabbed from form id=myForm on html
     //able to receive new location information, and loop into the rest of the page
 
-
-//total hourly sales
+    
+    //total hourly sales
     //for loop for hours the store is open
-        //multiplies customers *  avg purchse for openHoursArr.length
+    //multiplies customers *  avgPurchase purchse for openHoursArr.length
 //add up each hour's sales together to get var dayTotalSales
 
 //add to table id=myTable of html:
     //th (table head) 
-        //td enter table data into table head
+    //td enter table data into table head
             //data should be the name of location
             //td the amount sold each hour, with a different tr
         //append the data to the html
           
-//render page
+        
+        
+//OBJECT INSTANCES----------------------------------------
+new Locationbio('bob', 53, 12, 5.2);
+new Locationbio('john', 5, 2, 0.5);
+
+//FORM------------------------------------------->
+//make prototype to autofill (from instance)
+var formEl = document.getElementById("myForm");
+
+formEl.addEventListener("submit", function(event){
+    event.preventDefault();
+    var name = event.target.name.value
+    var maxCustomers = event.target.maxCustomers.value
+    // console.log(maxCustomers);
+    var minCustomers = event.target.minCustomers.value
+    // console.log(minCustomers);
+    var avgPurchase = event.target.avgPurchase.value
+
+    var anotherstore = new Locationbio(name, maxCustomers, minCustomers, avgPurchase)
+    console.log(anotherstore);
+});
+
+//render page --------------------------------------
+Locationbio.prototype.render=function(){
+    this.generateCustomerArray();
+    this.generateSalesArray();
+
+};
+
+for(var i=0; i<allLocationsArr.length; i++){
+allLocationsArr[i].render();
+};
+//END PAGE----------------------------------------------------
